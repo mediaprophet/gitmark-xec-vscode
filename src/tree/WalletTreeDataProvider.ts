7d0adfaeef48eed7bb7f11032a6ef6d22fc7f687
mediaprophet <34872169+mediaprophet@users.noreply.github.com>
2025-07-22 16:12:44 +1000

    fix chronik

--- src/tree/WalletTreeDataProvider.ts
+++ src/tree/WalletTreeDataProvider.ts
@@ -4,8 +4,17 @@ import { Wallet } from 'ecash-wallet'; // Import the Wallet class
 
 // The constructor now expects an array of URLs.
 const chronik = new ChronikClient([
+    'https://chronik.cash/',
+    'https://chronik.e.cash',
     'https://chronik.be.cash/xec',
-    'https://chronik.fabien.cash'
+    'https://chronik.fabien.cash',
+    'https://chronik-native2.fabien.cash',
+    'https://chronik-native3.fabien.cash',
+    'https://chronik.pay2stay.com/xec2',
+    'https://chronik-native1.fabien.cash',
+    'https://chronik1.alitayin.com',
+    'https://chronik2.alitayin.com'
+ 
 ]);
 
 interface WalletInfo {
